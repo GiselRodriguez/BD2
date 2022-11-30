c90df77e14ba0473e454e79fe4d8bfa89f760ed8
Gisel Rodriguez <116655556+GiselRodriguez@users.noreply.github.com>
2022-11-30 20:22:55 -0300

    Update 01.sql

--- exercicios-4/01.sql
+++ exercicios-4/01.sql
@@ -1,1 +1,16 @@
+use world;
 
+SELECT
+country.Name,
+count(countrylanguage.Language) AS NumLinguas
+FROM country INNER JOIN countrylanguage ON
+country.Code = countrylanguage.CountryCode
+GROUP BY country.Name
+HAVING(count(countrylanguage.Language) >
+(SELECT 
+avg(Linguas) FROM
+(select count(countrylanguage.Language) as Linguas
+from country
+inner join countrylanguage 
+on country.Code = countrylanguage.CountryCode
+group by country.Name) as Lingua));
